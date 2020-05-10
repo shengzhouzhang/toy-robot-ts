caffa068822eac719b47614bf0994730dcd826df
Steven Zhang <szhang@seek.com.au>
2020-05-10 22:19:24 +1000

    Refactoring

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -1,5 +1,5 @@
-import { Direction } from '../../directions/directions';
-import { DimensionX, DimensionY } from '../../table/table';
+import { Direction } from '../directions/directions';
+import { DimensionX, DimensionY } from '../table/table';
 
 export type Place = {
   kind: 'Place';
