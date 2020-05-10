a833800c7f8ddfe2995bb3c35cd20b6962694950
Steven Zhang <szhang@seek.com.au>
2020-05-10 21:21:54 +1000

    Refactor direction

--- src/directions/directions.spec.ts
+++ src/directions/directions.spec.ts
@@ -1,25 +1,32 @@
 import { Direction, rotateLeft, rotateRight } from './directions';
 
 describe('direction', () => {
+  type Rotate = {
+    direction: Direction;
+    expected: Direction;
+  };
+
   describe('rotate left', () => {
-    test.each([
-      ['North', 'West'],
-      ['West', 'South'],
-      ['South', 'East'],
-      ['East', 'North'],
-    ])('should rotate %s to %s', (direction: Direction, expected: Direction) => {
+    test.each`
+      direction  | expected
+      ${'North'} | ${'West'}
+      ${'West'}  | ${'South'}
+      ${'South'} | ${'East'}
+      ${'East'}  | ${'North'}
+    `('should rotate $direction to $expected', ({ direction, expected }: Rotate) => {
       const result = rotateLeft(direction);
       expect(result).toBe(expected);
     });
   });
 
   describe('rotate right', () => {
-    test.each([
-      ['North', 'East'],
-      ['East', 'South'],
-      ['South', 'West'],
-      ['West', 'North'],
-    ])('should rotate %s to %s', (direction: Direction, expected: Direction) => {
+    test.each`
+      direction  | expected
+      ${'North'} | ${'East'}
+      ${'West'}  | ${'North'}
+      ${'South'} | ${'West'}
+      ${'East'}  | ${'South'}
+    `('should rotate %s to %s', ({ direction, expected }: Rotate) => {
       const result = rotateRight(direction);
       expect(result).toBe(expected);
     });
